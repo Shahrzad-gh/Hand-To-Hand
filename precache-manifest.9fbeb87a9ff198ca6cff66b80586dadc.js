22bc8df5a260937443cae49c581ea656632c3c4e
Shahrzad GhahvehRokhi <gh.shahrzad@gmail.com>
2020-09-29 14:46:52 +0330

    Updates

---
+++ precache-manifest.9fbeb87a9ff198ca6cff66b80586dadc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406c113c475287eed9942df6491931f4",
+    "url": "/Hand-To-Hand/index.html"
+  },
+  {
+    "revision": "d796e7a91be3c524354d",
+    "url": "/Hand-To-Hand/static/css/2.0b0521d5.chunk.css"
+  },
+  {
+    "revision": "59a46402862b4aad2ab7",
+    "url": "/Hand-To-Hand/static/css/main.3683c902.chunk.css"
+  },
+  {
+    "revision": "d796e7a91be3c524354d",
+    "url": "/Hand-To-Hand/static/js/2.4ab0f1ba.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/Hand-To-Hand/static/js/2.4ab0f1ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59a46402862b4aad2ab7",
+    "url": "/Hand-To-Hand/static/js/main.11624725.chunk.js"
+  },
+  {
+    "revision": "7096f55eb1a8ca548662",
+    "url": "/Hand-To-Hand/static/js/runtime-main.012aee30.js"
+  }
+]);
