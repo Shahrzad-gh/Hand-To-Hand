29a8870d32e4374b5a5cc21e54151650e92ded3f
Shahr Zad <gh.shahrzad@gmail.com>
2022-01-27 12:36:39 +0330

    Updates

---
+++ precache-manifest.180ef4e0bb6b9db031e424700e02c081.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be23d5bc1394c41127d1639f72f22c02",
+    "url": "/Hand-To-Hand/index.html"
+  },
+  {
+    "revision": "f82608d836513802a753",
+    "url": "/Hand-To-Hand/static/css/2.0b0521d5.chunk.css"
+  },
+  {
+    "revision": "c2ba73dcc83f53cb8368",
+    "url": "/Hand-To-Hand/static/css/main.3351a2c7.chunk.css"
+  },
+  {
+    "revision": "f82608d836513802a753",
+    "url": "/Hand-To-Hand/static/js/2.ffa46633.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/Hand-To-Hand/static/js/2.ffa46633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2ba73dcc83f53cb8368",
+    "url": "/Hand-To-Hand/static/js/main.85dad752.chunk.js"
+  },
+  {
+    "revision": "7096f55eb1a8ca548662",
+    "url": "/Hand-To-Hand/static/js/runtime-main.012aee30.js"
+  }
+]);
