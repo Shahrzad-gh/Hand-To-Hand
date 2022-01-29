55f5f3e9f4114e6fe0df75804980b901f141a347
Shahr Zad <gh.shahrzad@gmail.com>
2022-01-29 20:23:39 +0330

    add story details css

--- src/components/Story/Story.js
+++ src/components/Story/Story.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./Story.scss";
 
 function Story() {
-  return <div className="story"></div>;
+  return (
+    <div className="story">
+      <img
+        src="https://image.freepik.com/free-vector/flat-design-summer-party-flyer_23-2148526263.jpg"
+        alt="story"
+      />
+      <div className="profile-pic">
+        <img
+          src="https://img.freepik.com/free-photo/image-magnetic-handsome-helpless-young-man-shrugging-with-shoulders-looking-directly-raising-arms_176532-10250.jpg?size=338&ext=jpg&ga=GA1.2.94874757.1637489236"
+          alt="profile pic"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Story;
