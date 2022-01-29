68575559c2ca71afb40494e15099a54e9027ebe8
Shahr Zad <gh.shahrzad@gmail.com>
2022-01-29 15:56:56 +0330

    add story

---
+++ src/components/Story/Story.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Story.scss";
+
+function Story() {
+  return <div className="story"></div>;
+}
+
+export default Story;
---
+++ src/components/Story/StoryList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Story from "./Story";
+import "./Story.scss";
+
+function StoryList({ data }) {
+  return (
+    <div className="storyList">
+      {/* {data.map((story, index) => ( */}
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+      {/* ))} */}
+    </div>
+  );
+}
+
+export default StoryList;
